f89eb868711d1bd245fd7d6a1354800c7c54e206
unknown <shabankhawar99@gmail.com>
2020-07-16 17:36:21 +0500

    dynamic types

--- app.ts
+++
@@ -1,1 +0,0 @@
-let app = ["name", 90]
---
+++ sandbox.js
@@ -0,0 +1,17 @@
+var person = /** @class */ (function () {
+    function person() {
+        this.id = 0;
+        this.licensed = false;
+    }
+    person.prototype.show = function () {
+        console.log(this.id + " " + this.licensed);
+    };
+    return person;
+}());
+var age = 26;
+var myName = "wololo";
+var breaker = "ayoyo";
+breaker = 12;
+breaker = true;
+var micah = new person();
+micah.show();
