c6c56d9122bb193559306915d03ffba0ce9d5073
unknown <shabankhawar99@gmail.com>
2020-07-16 19:24:34 +0500

    covered function basics

--- public/second.js
+++ public/second.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("worked here as well");
+var foo;
+foo = function () {
+    return 2;
+};
+var fun = function (a, b, c) {
+    if (c === void 0) { c = 20; }
+    console.log(a + b);
+    console.log(c);
+};
+fun(21, 21, "okay");
+var minus = function (a, b) {
+    return a - b;
+};
+console.log(minus(10, 20));
